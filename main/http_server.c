c697ac1791da3594a810c6631805d1850c927011
vaca <wsmslgh@gmail.com>
2022-04-11 06:54:04 -0700

    Initial commit

---
+++ main/http_server.c
@@ -0,0 +1,180 @@
+/* Simple HTTP Server Example
+
+   This example code is in the Public Domain (or CC0 licensed, at your option.)
+
+   Unless required by applicable law or agreed to in writing, this
+   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+   CONDITIONS OF ANY KIND, either express or implied.
+*/
+
+#include <esp_wifi.h>
+#include <esp_event.h>
+#include <esp_log.h>
+#include <esp_system.h>
+#include <esp_timer.h>
+#include <sys/param.h>
+//#include "nvs_flash.h"
+#include "esp_netif.h"
+//#include "esp_eth.h"
+//#include "protocol_examples_common.h"
+
+#include <esp_http_server.h>
+
+#include "pages.h"
+#include "main_app.h"
+#include "router_globals.h"
+
+static const char *TAG = "HTTPServer";
+
+esp_timer_handle_t restart_timer;
+
+static void restart_timer_callback(void *arg) {
+    ESP_LOGI(TAG, "Restarting now...");
+    esp_restart();
+}
+
+esp_timer_create_args_t restart_timer_args = {
+        .callback = &restart_timer_callback,
+        /* argument specified here will be passed to timer callback function */
+        .arg = (void *) 0,
+        .name = "restart_timer"
+};
+
+/* An HTTP GET handler */
+static esp_err_t index_get_handler(httpd_req_t *req) {
+    char *buf;
+    size_t buf_len;
+
+    /* Get header value string length and allocate memory for length + 1,
+     * extra byte for null termination */
+    buf_len = httpd_req_get_hdr_value_len(req, "Host") + 1;
+    if (buf_len > 1) {
+        buf = malloc(buf_len);
+        /* Copy null terminated value string into buffer */
+        if (httpd_req_get_hdr_value_str(req, "Host", buf, buf_len) == ESP_OK) {
+            ESP_LOGI(TAG, "Found header => Host: %s", buf);
+        }
+        free(buf);
+    }
+
+    /* Read URL query string length and allocate memory for length + 1,
+     * extra byte for null termination */
+    buf_len = httpd_req_get_url_query_len(req) + 1;
+    if (buf_len > 1) {
+        buf = malloc(buf_len);
+        if (httpd_req_get_url_query_str(req, buf, buf_len) == ESP_OK) {
+            ESP_LOGI(TAG, "Found URL query => %s", buf);
+            if (strcmp(buf, "reset=Restart") == 0) {
+                esp_timer_start_once(restart_timer, 500000);
+            }
+            char param1[64];
+            /* Get value of expected key from query string */
+            if (httpd_query_key_value(buf, "ap_ssid", param1, sizeof(param1)) == ESP_OK) {
+                preprocess_string(param1);
+                ESP_LOGI(TAG, "Found URL query parameter => ap_ssid=%s  %d", param1, strlen(param1));
+                set_str("ap_ssid",param1);
+            }
+            if (httpd_query_key_value(buf, "ap_password", param1, sizeof(param1)) == ESP_OK) {
+                preprocess_string(param1);
+                ESP_LOGI(TAG, "Found URL query parameter => ap_password=%s  %d", param1, strlen(param1));
+                set_str("ap_password",param1);
+            }
+            if (httpd_query_key_value(buf, "ble_name", param1, sizeof(param1)) == ESP_OK) {
+                preprocess_string(param1);
+                ESP_LOGI(TAG, "Found URL query parameter => ble_name=%s  %d", param1, strlen(param1));
+                set_str("ble_name",param1);
+            }
+            if (httpd_query_key_value(buf, "is_connect_router", param1, sizeof(param1)) == ESP_OK) {
+                preprocess_string(param1);
+                ESP_LOGI(TAG, "Found URL query parameter => is_connect_router=%s  %d", param1, strlen(param1));
+                set_str("is_connect_router",param1);
+            }
+            if (httpd_query_key_value(buf, "router_name", param1, sizeof(param1)) == ESP_OK) {
+                preprocess_string(param1);
+                ESP_LOGI(TAG, "Found URL query parameter => router_name=%s  %d", param1, strlen(param1));
+                set_str("router_name",param1);
+            }
+            if (httpd_query_key_value(buf, "router_password", param1, sizeof(param1)) == ESP_OK) {
+                preprocess_string(param1);
+                ESP_LOGI(TAG, "Found URL query parameter => router_password=%s  %d", param1, strlen(param1));
+                set_str("router_password",param1);
+            }
+            if (httpd_query_key_value(buf, "inner_ip", param1, sizeof(param1)) == ESP_OK) {
+                preprocess_string(param1);
+                ESP_LOGI(TAG, "Found URL query parameter => inner_ip=%s  %d", param1, strlen(param1));
+                set_str("inner_ip",param1);
+            }
+            if (httpd_query_key_value(buf, "tcp_port", param1, sizeof(param1)) == ESP_OK) {
+                preprocess_string(param1);
+                ESP_LOGI(TAG, "Found URL query parameter => tcp_port=%s  %d", param1, strlen(param1));
+                set_str("tcp_port",param1);
+            }
+            if (httpd_query_key_value(buf, "udp_port", param1, sizeof(param1)) == ESP_OK) {
+                preprocess_string(param1);
+                ESP_LOGI(TAG, "Found URL query parameter => udp_port=%s  %d", param1, strlen(param1));
+                set_str("udp_port",param1);
+            }
+            if (httpd_query_key_value(buf, "bau", param1, sizeof(param1)) == ESP_OK) {
+                preprocess_string(param1);
+                ESP_LOGI(TAG, "Found URL query parameter => bau=%s  %d", param1, strlen(param1));
+                set_str("bau",param1);
+            }
+
+
+        }
+        free(buf);
+    }
+
+    /* Send response with custom headers and body set as the
+     * string passed in user context*/
+    const char *resp_str = (const char *) req->user_ctx;
+    httpd_resp_send(req, resp_str, strlen(resp_str));
+
+    return ESP_OK;
+}
+
+static httpd_uri_t indexp = {
+        .uri       = "/",
+        .method    = HTTP_GET,
+        .handler   = index_get_handler,
+};
+
+esp_err_t http_404_error_handler(httpd_req_t *req, httpd_err_code_t err) {
+    httpd_resp_send_err(req, HTTPD_404_NOT_FOUND, "Page not found");
+    return ESP_FAIL;
+}
+
+httpd_handle_t start_webserver(void) {
+    httpd_handle_t server = NULL;
+    httpd_config_t config = HTTPD_DEFAULT_CONFIG();
+    char ble_name[] = "dada";
+    char checked[] = "checked";
+    int tcp_port= 83;
+    int udp_port = 82;
+    char bau[] = "115200";
+
+    const char *config_page_template = CONFIG_PAGE;
+    char *config_page = malloc(strlen(config_page_template) + 512);
+    sprintf(config_page, config_page_template, ap_ssid, ap_passwd, ble_name, checked, ssid, passwd,
+            static_ip, tcp_port, udp_port, bau);
+    indexp.user_ctx = config_page;
+
+    esp_timer_create(&restart_timer_args, &restart_timer);
+
+    // Start the httpd server
+    ESP_LOGI(TAG, "Starting server on port: '%d'", config.server_port);
+    if (httpd_start(&server, &config) == ESP_OK) {
+        // Set URI handlers
+        ESP_LOGI(TAG, "Registering URI handlers");
+        httpd_register_uri_handler(server, &indexp);
+        return server;
+    }
+
+    ESP_LOGI(TAG, "Error starting server!");
+    return NULL;
+}
+
+static void stop_webserver(httpd_handle_t server) {
+    // Stop the httpd server
+    httpd_stop(server);
+}
---
+++ main/pages.h
@@ -0,0 +1,132 @@
+#ifndef HTTP_SERVER1_PAGE_H
+#define HTTP_SERVER1_PAGE_H
+#define CONFIG_PAGE "\
+<!doctype html>\
+<html>\
+<head>\
+<meta charset='utf-8'>\
+<meta content='width=device-width,initial-scale=1' name='viewport'>\
+<title>ESP32 OV2460</title>\
+<style>\
+body {\
+font-family: Arial, Helvetica, sans-serif;\
+background: #ffffff;\
+color: #000000;\
+font-size: 16px\
+}\
+h2 {\
+font-size: 18px\
+}\
+section.main {\
+margin-top: 30px;\
+justify-content: center;\
+display: flex\
+}\
+.n1 {\
+height: 25px;\
+}\
+.n5 {\
+height: 100px;\
+}\
+.save {\
+margin-top: 50px;\
+margin-right: 50px;\
+height: 60px;\
+width: 120px;\
+font-size: large;\
+}\
+td {\
+width: 130px;\
+padding: 5px;\
+horiz-align: left\
+}\
+.spacer {\
+height: 20px;\
+}\
+tr {\
+text-align: right\
+}\
+</style>\
+</head>\
+<body>\
+<section class='main'>\
+<div id='content'>\
+<script>\
+if (window.location.search.substr(1) !== '') {\
+document.getElementById('content').display = 'none';\
+document.body.innerHTML = '<h1>ESP32串口透传配置</h1>设置成功， 请等待...';\
+setTimeout('location.href = '/'', 10000);\
+}\
+</script>\
+<form action='' method='get'>\
+<table>\
+<tr>\
+<td>WiFi名称</td>\
+<td><input class='n1' id='wifi_name' name='ap_ssid' type='text' value='%s'/></td>\
+</tr>\
+<tr>\
+<td>WiFi密码</td>\
+<td><input class='n1' id='wifi_password' name='ap_password' type='text' value='%s'/></td>\
+</tr>\
+<tr class='n5'>\
+<td>蓝牙名称</td>\
+<td><input class='n1' id='ble_name' name='ble_name' type='text' value='%s'/></td>\
+</tr>\
+<tr>\
+<td></td>\
+<td>\
+<div style='float:left;'><input %s id='n3' id='is_connect_router'\
+name='is_connect_router'\
+type='checkbox'></div>\
+<div style='float:left;'>是否连接路由器</div>\
+</td>\
+</tr>\
+<tr>\
+<td>路由器名称</td>\
+<td><input class='n1' id='router_name' name='router_name' type='text' value='%s'/></td>\
+</tr>\
+<tr>\
+<td>路由器密码</td>\
+<td><input class='n1' id='router_password' name='router_password' type='text' value='%s'/></td>\
+</tr>\
+<tr>\
+<td>内网IP</td>\
+<td><input class='n1' id='inner_ip' name='inner_ip' type='text' value='%s'/></td>\
+</tr>\
+<tr class='spacer'></tr>\
+<tr>\
+<td>TCP Server 端口</td>\
+<td><input class='n1' id='tcp_port' name='tcp_port' type='number' value='%d'/></td>\
+</tr>\
+<tr>\
+<td>UDP Server 端口</td>\
+<td><input class='n1' id='udp_port' name='udp_port' type='number' value='%d'/></td>\
+</tr>\
+<tr class='spacer'></tr>\
+<tr>\
+<td>串口波特率</td>\
+<td>\
+<select class='n2' class='default-action' id='bau' name='bau'>\
+<option value='' selected='selected' hidden='hidden'>%s</option>\
+<option value='1'>9600</option>\
+<option value='2'>19200</option>\
+<option value='3'>38400</option>\
+<option value='4'>115200</option>\
+<option value='5'>2000000</option>\
+</select>\
+</td>\
+</tr>\
+<tr>\
+<td></td>\
+<td>\
+<div style='float:left;'><input class='save' id='save' type='submit' value='保存'/></div>\
+</td>\
+</tr>\
+</table>\
+</form>\
+</div>\
+</section>\
+</body>\
+</html>\
+"
+#endif //HTTP_SERVER1_PAGE_H
